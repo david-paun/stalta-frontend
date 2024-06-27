0634d6a5702a9f2e26c8c0a02acceebff67dbc14
David Paunovski <paunovskidavid@gmail.com>
2024-06-27 04:42:16 +0200

    ci pipeline dockerhub

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../core/ui/header/header.component";
+import { FooterComponent } from "../../core/ui/footer/footer.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [HeaderComponent, FooterComponent]
 })
 export class HomeComponent {
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     './src/app/**/*.{html,js}',
     './src/app/index.html',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['"Open Sans"', 'sans-serif'],
+    },
+    extend: {    
+    },
   },
   plugins: [],
 }
